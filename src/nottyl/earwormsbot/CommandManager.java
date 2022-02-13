f81d5fa456bb9750afbdbb06289a8dd28bebb84e
not tyler <71718049+nottyl@users.noreply.github.com>
2022-02-13 23:51:36 +0800

    Fix Command Manager

--- src/nottyl/earwormsbot/CommandManager.java
+++ src/nottyl/earwormsbot/CommandManager.java
@@ -4,20 +4,23 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import nottyl.earwormsbot.commands.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class CommandManager {
-    public final static  List<ICommand> commands = new ArrayList<>();
+    public final static  List<ICommand> commands = new ArrayList<>(
+            Arrays.asList(
+                    new Hello(),
+                    new Disconnect(),
+                    new Join(),
+                    new Play(),
+                    new Pause(),
+                    new Resume(),
+                    new Stop()
+            )
+    );
 
-    public CommandManager() {
-        addCommand(new Hello());
-        addCommand(new Join());
-        addCommand(new Play());
-        addCommand(new Pause());
-        addCommand(new Resume());
-        addCommand(new Stop());
-    }
 
     public static void handle(MessageCreateEvent event) {
         String query = event.getMessage().getContent().substring(1);
@@ -25,17 +28,6 @@ public class CommandManager {
             if(query.equalsIgnoreCase(cmd.name())){
                 cmd.execute(event);
             }
-        }
-    }
-
-    private void addCommand(ICommand cmd) {
-        boolean nameFound = commands.stream()
-                .anyMatch((x) -> x.name().equalsIgnoreCase(cmd.name()));
-
-        if (nameFound) {
-            throw new IllegalArgumentException("This command is already existing.");
-        } else {
-            commands.add(cmd);
         }
     }
 
